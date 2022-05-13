a2b7b5cc694008a9d7822fd7af7297f7aca5ca4f
henriquepadua <henrique.lommes@gmail.com>
2022-05-13 15:10:11 -0300

    fila flexivel

---
+++ TP3/QT13.java
@@ -0,0 +1,433 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+
+//Classe Filmes
+class Filme{
+  SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+   private String Nome;
+   private String Titulo;
+   private Date Datadelancamento;
+   private int Duracao;
+   private String Genero;
+   private String Idioma;
+   private String Situacao;
+   private float Orcamento;
+   private String[] Palavrachave;
+
+   //Construtores
+   public Filme(){
+      Nome = "";
+      Titulo = "";
+      Duracao = 0;
+      Genero = "";
+      Idioma = "";
+      Situacao = "";
+      Orcamento = 0;
+      Palavrachave = new String[1000];
+   }
+   
+   public Filme(String Nome,String Titulo,Date Datadelancamento,int Duracao,String Genero,String Idioma,String Situacao,float Orcamento,String[] Palavrachave){
+      this.Nome = Nome;
+      this.Titulo = Titulo;
+      this.Datadelancamento = Datadelancamento;
+      this.Duracao = Duracao;
+      this.Genero = Genero;
+      this.Idioma = Idioma;
+      this.Situacao = Situacao;
+      this.Orcamento = Orcamento;
+      this.Palavrachave = Palavrachave;
+   }
+
+   //Metodos sets
+   public void setNome(String Nome){this.Nome = Nome;};
+   public void setTitulo(String Titulo){this.Titulo = Titulo;};
+   public void setDatalancamento(Date Datadelancamento){this.Datadelancamento = Datadelancamento;};
+   public void setDuracao(int Duracao){this.Duracao = Duracao;};
+   public void setGenero(String Genero){this.Genero = Genero;};
+   public void setIdioma(String Idioma){this.Idioma = Idioma;};
+   public void setSituacao(String Situacao){this.Situacao = Situacao;};
+   public void setOrcamento(float Orcamento){this.Orcamento = Orcamento;};
+   public void setPalavrachave(String[] Palavrachave){this.Palavrachave = Palavrachave;};
+
+   //Metodos gets
+   public String getNome(){return this.Nome;};
+   public String getTitulo(){return this.Titulo;};
+   public Date getDatadelancamento(){
+    if(this.Datadelancamento == null)
+       return new Date();     
+      return this.Datadelancamento;
+    }
+   public int getDuracao(){return this.Duracao;};
+   public String getGenero(){return this.Genero;};
+   public String getIdioma(){return this.Idioma;};
+   public String getSituacao(){return this.Situacao;};
+   public float getOrcamento(){return this.Orcamento;};
+   public String[] getPalavrachave(){return this.Palavrachave;};   
+   public String getPalavrachaveString(){
+    String palavraschave = "";
+    //MyIO.print("[");
+    for(String s:this.Palavrachave){
+      palavraschave +=  s + "," +" ";
+      if(s == null){
+        break;
+      }
+    }
+    palavraschave = palavraschave.replaceAll(", null,", "");
+   // MyIO.print("]");
+    palavraschave.trim();
+    return palavraschave.trim();
+  }
+
+ 
+   //Metodo clone
+   public void Clone(){
+     Filme filmes = new Filme();
+        filmes.setNome(this.Nome);
+        filmes.setTitulo(this.Titulo);
+        filmes.setDatalancamento(this.Datadelancamento);
+        filmes.setDuracao(this.Duracao);
+        filmes.setGenero(this.Genero);
+        filmes.setIdioma(this.Idioma);
+        filmes.setSituacao(this.Situacao);
+        filmes.setOrcamento(this.Orcamento);
+        filmes.setPalavrachave(this.Palavrachave);
+   }
+
+   public String removeTags(String line){
+    String resp = "";
+    int i = 0;
+    while(i < line.length()){ 
+        if(line.charAt(i) == '<'){ 
+            i++;
+            while(line.charAt(i) != '>') i++;
+        } else if(line.charAt(i) == '&'){ 
+          i++;
+          while(line.charAt(i) != ';') i++;
+      } else { 
+            resp += line.charAt(i);
+        }
+        i++;
+    }
+    return resp;
+   }
+
+   public static String pegarIdioma(String limpa){
+    limpa = limpa.replaceAll("Idioma original", "");
+    return limpa;
+   }
+
+   public static String pegarTitulo(String limpa){
+    limpa = limpa.replaceAll("Título original", "");
+    return limpa;
+   }
+
+   public static String pegarSituacao(String limpa){
+     limpa = limpa.replaceAll("Situação", "");
+     return limpa;
+   }
+
+   public static String pegarOrcamento(String limpa){
+     limpa = limpa.replaceAll("Orçamento $", "");
+     return limpa;
+   }
+
+   public static int pegarDuracao(String limpa){
+     String line = "";
+     line = limpa.trim();
+     int duracao = 0;
+     
+     if(line.contains("h")){
+       duracao += Integer.parseInt(line.charAt(0)+"")*60;
+       if(line.contains("m")){
+         line.trim();
+         String minutos = line.substring(3, line.indexOf("m"));
+         duracao += Integer.parseInt(minutos);
+       }
+     }
+
+     else if(line.contains("m")){
+       String minutos = line.substring(0, (line.indexOf("m")));
+       duracao += Integer.parseInt(minutos);
+     }
+     return duracao;
+   }
+
+   public static String buscarAteParenteses(String filename){
+     String line = "";
+
+    for(int i = 0;filename.charAt(i) != '('; i++){
+        line += filename.charAt(i);
+    }    
+     return line;
+   }
+
+   private String removeLetters(String value){
+      // Data declaration
+      String result = "";
+
+      for(int i = 0; i < value.length(); i++){
+          // If char is a number, a blank space, or a '.' (Used on convertBudget), will be stored into "result"
+          if( (value.charAt(i) >= 48 && value.charAt(i) <= 57) || value.charAt(i) == ' ' || value.charAt(i) == '.')
+              result += value.charAt(i);
+      }
+      return result;
+   } 
+
+   private Float convertBudget(String value){
+        return Float.parseFloat(removeLetters(value));
+     }
+
+
+
+   public void lerHtml(String filename) throws ParseException{   
+     String line = "";
+     String verde = "/tmp/filmes/" + filename;
+     String teste = "/Users/User/Documents/TPS/TP3/" + filename;
+     //String teste2 = "/Users/1325905/Documents/TP2/" + filename;
+
+     try{
+      FileReader reader = new FileReader(teste);
+      BufferedReader buffer = new BufferedReader(reader);
+      
+          line  = buffer.readLine();
+          while(!line.contains("<title>")){
+            line = buffer.readLine();
+          }
+
+          this.setNome((buscarAteParenteses(removeTags(line))).trim());
+
+          //line = buffer.readLine();
+          while(!line.contains("span class=\"release\"")){
+            line = buffer.readLine();
+          }
+          line = buffer.readLine();
+          this.setDatalancamento(sdf.parse(buscarAteParenteses(line).trim()));
+
+          line = buffer.readLine();
+          while(!line.contains("span class=\"genres\"")){
+            line = buffer.readLine();
+          }
+          line = buffer.readLine();
+          line = buffer.readLine();
+          this.setGenero(removeTags(line).trim());
+
+          line = buffer.readLine();
+          while(!line.contains("span class=\"runtime\"")){
+            line = buffer.readLine();
+          }
+          line = buffer.readLine();
+          line = buffer.readLine();
+          this.setDuracao(pegarDuracao((line)));
+
+
+          line = buffer.readLine();
+          while(!line.contains("p class=\"wrap\"") && (!line.contains("<strong><bdi>"))){
+            line = buffer.readLine();
+          }
+          if(line.contains("p class=\"wrap\"")){
+            this.setTitulo(removeTags(pegarTitulo(line)).trim());
+          }
+          if(line.contains("<strong><bdi>")){
+            this.Titulo = this.Nome;
+          }
+
+          while(!line.contains("<strong><bdi>")){
+            line = buffer.readLine();
+          }
+          this.setSituacao(removeTags(pegarSituacao(line)).trim());    
+
+          line = buffer.readLine();
+          while(!line.contains("<p><strong>")){
+            line = buffer.readLine();
+          }
+          this.setIdioma(removeTags(pegarIdioma(line)).trim());
+
+          line = buffer.readLine();
+          while(!line.contains("Orçamento</bdi>"));
+            String aux = removeTags(line.replace("Orçamento", " ")).trim();
+            this.setOrcamento((aux.equals("-")) ? 0.0F : convertBudget(aux));
+         
+          String[] entrada = new String[30];
+          int cont = 0;
+          line = buffer.readLine();
+          while(!line.contains("</ul>")){
+            line = buffer.readLine();
+            if(line.contains("<li>")){
+              entrada[cont++] = removeTags(line).trim();   
+            }
+          }
+          cont = cont>0?cont-1:0;
+          Palavrachave = new String[cont];
+          this.setPalavrachave(entrada);
+
+       buffer.close();
+     }catch(FileNotFoundException e){
+      e.printStackTrace();
+     }catch(ParseException e){
+      e.getLocalizedMessage();
+     }catch(IOException e){
+      System.out.println("Nao consigo ler o arquivo");
+     }
+   }
+
+   public String imprimir(){
+     return (getNome() + " " + getTitulo()+ " " + sdf.format(getDatadelancamento())+ " " + getDuracao() + " " + getGenero() + " " + getIdioma() + " " +getSituacao() + " " +getOrcamento() + " [" + getPalavrachaveString() + "]");  
+   }
+
+}//fechando a classe Filmes
+
+class Celula {
+	public Filme elemento; // Elemento inserido na celula.
+	public Celula prox; // Aponta a celula prox.
+  public int cont = 0;
+
+	/**
+	 * Construtor da classe.
+	 */
+	public Celula() {
+		this(null);
+	}
+
+	/**
+	 * Construtor da classe.
+	 * @param elemento int inserido na celula.
+	 */
+	public Celula(Filme elemento) {
+      this.elemento = elemento;
+      this.prox = null;
+	}
+}
+
+
+class Fila {
+	private Celula primeiro;
+	private Celula ultimo;
+  public int cont;
+
+	/**
+	 * Construtor da classe que cria uma fila sem elementos (somente no cabeca).
+	 */
+  public Fila(){
+    cont = 0;
+    primeiro = new Celula();
+		ultimo = primeiro;
+  }
+
+
+	/**
+	 * Insere elemento na fila (politica FIFO).
+	 * @param x int elemento a inserir.
+	 * @throws Exception
+	 */
+	public void inserir(Filme x) throws Exception {
+    int j = tamanho();
+    if(j == 5){
+      primeiro.elemento = remover();
+    }
+		ultimo.prox = new Celula(x);
+		ultimo = ultimo.prox;
+    cont ++;
+    float soma = 0;
+    float media = 0;
+    for(Celula i = primeiro.prox; i != null; i = i.prox){
+      soma += i.elemento.getDuracao();
+    }
+    media = soma/cont;
+    int round = Math.round(media);
+    System.out.println(round);
+	}
+
+
+	/**
+	 * Remove elemento da fila (politica FIFO).
+	 * @return Elemento removido.
+	 * @trhows Exception Se a fila nao tiver elementos.
+	 */
+	public Filme remover() throws Exception {
+		if (primeiro == ultimo) {
+			throw new Exception("Erro ao remover!");
+		}
+    Celula tmp = primeiro;
+		primeiro = primeiro.prox;
+		Filme resp = primeiro.elemento;
+    tmp.prox = null;
+    tmp = null;
+    cont --;
+		return resp;
+	}
+  
+  public int tamanho() {
+    int tamanho = 0; 
+    for(Celula i = primeiro; i != ultimo; i = i.prox, tamanho++);
+    return tamanho;
+  }
+
+
+	/**
+	 * Mostra os elementos separados por espacos.
+	 */
+	public void mostrar() {
+		int j = 0;
+		for(Celula i = primeiro.prox; i != null; i = i.prox) {
+			System.out.print("[" + j + "] " + i.elemento.imprimir() + "\n");
+      j++;
+		}
+     
+		
+	}
+}
+
+
+class QT13{
+    public static void main(String[] args)throws Exception{
+        MyIO.setCharset("UTF-8");
+   
+         String[] leitura = new String[1000];
+         int Nentrada = 0,inteiro;
+          
+         String line = MyIO.readLine();
+         while(!line.contains("FIM")){
+           leitura[Nentrada++] = line;     
+           line = MyIO.readLine();
+         }
+   
+         Fila pegando = new Fila();
+         for(int i = 0 ; i < Nentrada;i++){
+           Filme filme = new Filme();
+           try{     
+             filme.lerHtml(leitura[i]); 
+             pegando.inserir(filme);
+           }catch(NullPointerException e){
+             e.printStackTrace();
+           }catch(ParseException e){
+             e.printStackTrace();
+           }catch(Exception e){
+             e.printStackTrace();
+           }  
+         }
+   
+        inteiro = MyIO.readInt(); 
+        for(int i = 0 ; i < inteiro;i++){
+         try{     
+           line = MyIO.readLine(); 
+           if(line.contains("I")){
+               Filme filme = new Filme();
+               filme.lerHtml(line.substring(2, line.length())); 
+               pegando.inserir(filme);
+           }else if(line.contains("R")){
+               Filme filme = pegando.remover();
+               System.out.println("(R) " + filme.getNome());
+           }
+         }catch(NullPointerException e){
+           e.printStackTrace();
+         }catch(ParseException e){
+           e.printStackTrace();
+         }catch(Exception e){
+           e.printStackTrace();
+         }
+        }
+        pegando.mostrar();
+      }
+   }
