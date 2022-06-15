9e171af45ceddc0ceaa43cb21197bcf191e5ab70
henriquepadua <henrique.lommes@gmail.com>
2022-06-15 18:04:08 -0300

    trabalhando com hash

---
+++ TP4/QT7.java
@@ -0,0 +1,467 @@
+import java.util.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.*;
+
+
+//Classe Filmes
+class Filme{
+  SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+   private String Nome;
+   private String Titulo;
+   private Date Datadelancamento;
+   private int Duracao;
+   private String Genero;
+   private String Idioma;
+   private String Situacao;
+   private float Orcamento;
+   private String[] Palavrachave;
+
+   //Construtores
+   public Filme(){
+      Nome = "";
+      Titulo = "";
+      Duracao = 0;
+      Genero = "";
+      Idioma = "";
+      Situacao = "";
+      Orcamento = 0;
+      Palavrachave = new String[1000];
+   }
+   
+   public Filme(String Nome,String Titulo,Date Datadelancamento,int Duracao,String Genero,String Idioma,String Situacao,float Orcamento,String[] Palavrachave){
+      this.Nome = Nome;
+      this.Titulo = Titulo;
+      this.Datadelancamento = Datadelancamento;
+      this.Duracao = Duracao;
+      this.Genero = Genero;
+      this.Idioma = Idioma;
+      this.Situacao = Situacao;
+      this.Orcamento = Orcamento;
+      this.Palavrachave = Palavrachave;
+   }
+
+   //Metodos sets
+   public void setNome(String Nome){this.Nome = Nome;};
+   public void setTitulo(String Titulo){this.Titulo = Titulo;};
+   public void setDatalancamento(Date Datadelancamento){this.Datadelancamento = Datadelancamento;};
+   public void setDuracao(int Duracao){this.Duracao = Duracao;};
+   public void setGenero(String Genero){this.Genero = Genero;};
+   public void setIdioma(String Idioma){this.Idioma = Idioma;};
+   public void setSituacao(String Situacao){this.Situacao = Situacao;};
+   public void setOrcamento(float Orcamento){this.Orcamento = Orcamento;};
+   public void setPalavrachave(String[] Palavrachave){this.Palavrachave = Palavrachave;};
+
+   //Metodos gets
+   public String getNome(){return this.Nome;};
+   public String getTitulo(){return this.Titulo;};
+   public Date getDatadelancamento(){
+    if(this.Datadelancamento == null)
+       return new Date();     
+      return this.Datadelancamento;
+    }
+   public int getDuracao(){return this.Duracao;};
+   public String getGenero(){return this.Genero;};
+   public String getIdioma(){return this.Idioma;};
+   public String getSituacao(){return this.Situacao;};
+   public float getOrcamento(){return this.Orcamento;};
+   public String[] getPalavrachave(){return this.Palavrachave;};   
+   public String getPalavrachaveString(){
+    String palavraschave = "";
+    //MyIO.print("[");
+    for(String s:this.Palavrachave){
+      palavraschave +=  s + "," +" ";
+      if(s == null){
+        break;
+      }
+    }
+    palavraschave = palavraschave.replaceAll(", null,", "");
+   // MyIO.print("]");
+    palavraschave.trim();
+    return palavraschave.trim();
+  }
+
+ 
+   //Metodo clone
+   public void Clone(){
+     Filme filmes = new Filme();
+        filmes.setNome(this.Nome);
+        filmes.setTitulo(this.Titulo);
+        filmes.setDatalancamento(this.Datadelancamento);
+        filmes.setDuracao(this.Duracao);
+        filmes.setGenero(this.Genero);
+        filmes.setIdioma(this.Idioma);
+        filmes.setSituacao(this.Situacao);
+        filmes.setOrcamento(this.Orcamento);
+        filmes.setPalavrachave(this.Palavrachave);
+   }
+
+   public String removeTags(String line){
+    String resp = "";
+    int i = 0;
+    while(i < line.length()){ 
+        if(line.charAt(i) == '<'){ 
+            i++;
+            while(line.charAt(i) != '>') i++;
+        } else if(line.charAt(i) == '&'){ 
+          i++;
+          while(line.charAt(i) != ';') i++;
+      } else { 
+            resp += line.charAt(i);
+        }
+        i++;
+    }
+    return resp;
+   }
+
+   public static String pegarIdioma(String limpa){
+    limpa = limpa.replaceAll("Idioma original", "");
+    return limpa;
+   }
+
+   public static String pegarTitulo(String limpa){
+    limpa = limpa.replaceAll("Título original", "");
+    return limpa;
+   }
+
+   public static String pegarSituacao(String limpa){
+     limpa = limpa.replaceAll("Situação", "");
+     return limpa;
+   }
+
+   public static String pegarOrcamento(String limpa){
+     limpa = limpa.replaceAll("Orçamento $", "");
+     return limpa;
+   }
+
+   public static int pegarDuracao(String limpa){
+     String line = "";
+     line = limpa.trim();
+     int duracao = 0;
+     
+     if(line.contains("h")){
+       duracao += Integer.parseInt(line.charAt(0)+"")*60;
+       if(line.contains("m")){
+         line.trim();
+         String minutos = line.substring(3, line.indexOf("m"));
+         duracao += Integer.parseInt(minutos);
+       }
+     }
+
+     else if(line.contains("m")){
+       String minutos = line.substring(0, (line.indexOf("m")));
+       duracao += Integer.parseInt(minutos);
+     }
+     return duracao;
+   }
+
+   public static String buscarAteParenteses(String filename){
+     String line = "";
+
+    for(int i = 0;filename.charAt(i) != '('; i++){
+        line += filename.charAt(i);
+    }    
+     return line;
+   }
+
+   private String removeLetters(String value){
+      // Data declaration
+      String result = "";
+
+      for(int i = 0; i < value.length(); i++){
+          // If char is a number, a blank space, or a '.' (Used on convertBudget), will be stored into "result"
+          if( (value.charAt(i) >= 48 && value.charAt(i) <= 57) || value.charAt(i) == ' ' || value.charAt(i) == '.')
+              result += value.charAt(i);
+      }
+      return result;
+   } 
+
+   private Float convertBudget(String value){
+        return Float.parseFloat(removeLetters(value));
+     }
+
+
+
+   public void lerHtml(String filename) throws ParseException{   
+     String line = "";
+     String verde = "/tmp/filmes/" + filename;
+     String teste = filename;
+     //String teste2 = "/Users/1325905/Documents/TP2/" + filename;
+
+     try{
+      FileReader reader = new FileReader(teste);
+      BufferedReader buffer = new BufferedReader(reader);
+      
+          line  = buffer.readLine();
+          while(!line.contains("<title>")){
+            line = buffer.readLine();
+          }
+
+          this.setNome((buscarAteParenteses(removeTags(line))).trim());
+
+          //line = buffer.readLine();
+          while(!line.contains("span class=\"release\"")){
+            line = buffer.readLine();
+          }
+          line = buffer.readLine();
+          this.setDatalancamento(sdf.parse(buscarAteParenteses(line).trim()));
+
+          line = buffer.readLine();
+          while(!line.contains("span class=\"genres\"")){
+            line = buffer.readLine();
+          }
+          line = buffer.readLine();
+          line = buffer.readLine();
+          this.setGenero(removeTags(line).trim());
+
+          line = buffer.readLine();
+          while(!line.contains("span class=\"runtime\"")){
+            line = buffer.readLine();
+          }
+          line = buffer.readLine();
+          line = buffer.readLine();
+          this.setDuracao(pegarDuracao((line)));
+
+
+          line = buffer.readLine();
+          while(!line.contains("p class=\"wrap\"") && (!line.contains("<strong><bdi>"))){
+            line = buffer.readLine();
+          }
+          if(line.contains("p class=\"wrap\"")){
+            this.setTitulo(removeTags(pegarTitulo(line)).trim());
+          }
+          if(line.contains("<strong><bdi>")){
+            this.Titulo = this.Nome;
+          }
+
+          while(!line.contains("<strong><bdi>")){
+            line = buffer.readLine();
+          }
+          this.setSituacao(removeTags(pegarSituacao(line)).trim());    
+
+          line = buffer.readLine();
+          while(!line.contains("<p><strong>")){
+            line = buffer.readLine();
+          }
+          this.setIdioma(removeTags(pegarIdioma(line)).trim());
+
+          line = buffer.readLine();
+          while(!line.contains("Orçamento</bdi>"));
+            String aux = removeTags(line.replace("Orçamento", " ")).trim();
+            this.setOrcamento((aux.equals("-")) ? 0.0F : convertBudget(aux));
+         
+          String[] entrada = new String[30];
+          int cont = 0;
+          line = buffer.readLine();
+          while(!line.contains("</ul>")){
+            line = buffer.readLine();
+            if(line.contains("<li>")){
+              entrada[cont++] = removeTags(line).trim();   
+            }
+          }
+          cont = cont>0?cont-1:0;
+          Palavrachave = new String[cont];
+          this.setPalavrachave(entrada);
+
+       buffer.close();
+     }catch(FileNotFoundException e){
+      e.printStackTrace();
+     }catch(ParseException e){
+      e.getLocalizedMessage();
+     }catch(IOException e){
+      System.out.println("Nao consigo ler o arquivo");
+     }
+   }
+
+   public String imprimir(){
+     return (getNome() + " " + getTitulo()+ " " + sdf.format(getDatadelancamento())+ " " + getDuracao() + " " + getGenero() + " " + getIdioma() + " " +getSituacao() + " " +getOrcamento() + " [" + getPalavrachaveString() + "]");  
+   }
+
+}//fechando a classe Filmes
+
+class Celula {
+	public String elemento; // Elemento inserido na celula.
+	public Celula prox; // Aponta a celula prox.
+
+	Celula(String elemento) {
+		this.elemento = elemento;
+		this.prox = null;
+	}
+
+	Celula(String elemento, Celula prox) {
+		this.elemento = elemento;
+		this.prox = prox;
+	}
+}
+
+class Lista {
+	private Celula primeiro; // Primeira celula: SEM elemento valido.
+	private Celula ultimo; // Ultima celula: COM elemento valido.
+
+	public Lista() {
+		primeiro = new Celula(null);
+		ultimo = primeiro;
+	}
+
+    public void mostrar() {
+		System.out.print("[ "); // Comeca a mostrar.
+		for (Celula i = primeiro.prox; i != null; i = i.prox) {
+			System.out.print(i.elemento + " ");
+		}
+		System.out.println("] "); // Termina de mostrar.
+	}
+
+	public boolean pesquisar(String x) {
+		boolean retorno = false;
+		for (Celula i = primeiro.prox; i != null; i = i.prox) {
+         if(i.elemento.compareTo(x) == 0){
+            retorno = true;
+            i = ultimo;
+         }
+		}
+		return retorno;
+	}
+
+	public void inserirInicio(String elemento) {
+		Celula tmp = new Celula(elemento);
+      tmp.prox = primeiro.prox;
+		primeiro.prox = tmp;
+		if (primeiro == ultimo) {
+			ultimo = tmp;
+		}
+      tmp = null;
+	}
+
+	public void inserirFim(String elemento) {
+		Celula tmp = new Celula(elemento);
+		ultimo.prox = tmp;
+		ultimo = ultimo.prox;
+      tmp = null;
+	}
+
+	public String removerInicio() throws Exception {
+      String resp = null;
+
+		if (primeiro == ultimo) {
+			throw new Exception("Erro ao remover (vazia)!");
+		}else{
+         primeiro = primeiro.prox;
+         resp = primeiro.elemento;
+      }
+
+		return resp;
+	}
+
+	public String removerFim() throws Exception {
+      String resp =  null;
+      Celula i = null;
+
+		if (primeiro == ultimo) {
+			throw new Exception("Erro ao remover (vazia)!");
+		} else {
+
+         resp = ultimo.elemento;
+
+		   // Caminhar ate a penultima celula:
+         for(i = primeiro; i.prox != ultimo; i = i.prox);
+
+         ultimo = i;
+         i = ultimo.prox = null;
+      }
+
+		return resp;
+	}
+}
+
+
+class HashIndiretoLista {
+    Lista tabela[];
+    int tamanho;
+    final int NULO = -1;
+ 
+    public HashIndiretoLista() {
+       this(7);
+    }
+ 
+    public HashIndiretoLista(int tamanho) {
+       this.tamanho = tamanho;
+       tabela = new Lista[tamanho];
+       for (int i = 0; i < tamanho; i++) {
+          tabela[i] = new Lista();
+       }
+    }
+ 
+    public int h(String elemento) {
+        String s = elemento;
+        int j = 0;
+        byte[] bytes = s.getBytes(StandardCharsets.US_ASCII);
+        for(int i = 0;i < elemento.length();i++){
+            j = j + bytes[i];     
+        }
+       return j % tamanho;
+    }
+ 
+    public boolean pesquisar(String elemento) {
+       boolean resp = false;
+       System.out.println("=>" + elemento);
+       int pos = h(elemento);
+       resp = tabela[pos].pesquisar(elemento);
+       if(resp == true){
+        System.out.println("Posicao :" + pos);
+       }
+       return resp;
+    }
+ 
+    public void inserirInicio(String elemento) {
+       int pos = h(elemento);
+       tabela[pos].inserirInicio(elemento);
+    }
+ 
+    public String remover(String elemento) throws Exception {
+       String resp = null;
+       if (pesquisar(elemento) == false) {
+          throw new Exception("Erro ao remover!");
+       } else {
+          int pos = h(elemento);
+          resp = tabela[pos].removerFim();
+       }
+       return resp;
+    }
+ }
+
+class QT7{
+    public static void main(String[] args)throws Exception{
+        MyIO.setCharset("ISO-8859-1");
+        Scanner sc = new Scanner(System.in);
+        String[] leitura = new String[1000];
+        int Nentrada = 0;
+  
+        String line = sc.nextLine();
+        while(!line.contains("FIM")){
+          leitura[Nentrada++] = line;
+          line = sc.nextLine();
+        }      
+        Filme filmes[] = new Filme[Nentrada];
+        HashIndiretoLista pegando = new HashIndiretoLista();
+        for(int i = 0 ; i < Nentrada;i++){
+          filmes[i] = new Filme();
+          try {
+          filmes[i].lerHtml(leitura[i]);
+          String s = filmes[i].getTitulo();
+          pegando.inserirInicio(s);
+          } catch (ParseException e) {
+          e.printStackTrace();
+          }
+        }
+  
+        line = sc.nextLine();
+        while(!line.contains("FIM")){
+          if(pegando.pesquisar(line) == true){
+              line = sc.nextLine();
+          }else{
+              System.out.println("NAO");
+              line = sc.nextLine();
+          }
+        }
+      }
+  } 
